5514534b2ea05ad5f7b05a01a426e2e8ee86e8cb
celsojr-websters <celso.j@websters.ae>
2024-10-22 18:51:59 +0400

    Add project files.

---
+++ BlazorApp/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string State { get; set; }
+        public decimal AnnualIncome { get; set; }
+    }
+}
