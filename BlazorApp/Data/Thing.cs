fa104c798e053932e87a0cba43f4fa63ba2c51a3
celsojr-websters <celso.j@websters.ae>
2024-10-25 18:54:27 +0400

    sqlite experiment

---
+++ BlazorApp/Data/Thing.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Data
+{
+    public class Thing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ BlazorApp/Data/ThingContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Data
+{
+    public class ThingContext : DbContext
+    {
+        public ThingContext(DbContextOptions<ThingContext> opts) : base(opts)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Thing>().HasData(
+                new Thing() { Id = 1, Name = "One thing" }
+            );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Thing> Things { get; set; } = null!;
+    }
+}
