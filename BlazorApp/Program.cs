6496db1d852da780592e854c53a5f848332fe4d4
celsojr-websters <96522139+celsojr-websters@users.noreply.github.com>
2024-10-26 19:00:01 +0400

    Load data test (#3)
    * Load data test

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using SqliteWasmHelper;
 
 namespace BlazorApp
-{
+{https://github.com/celsojr/BlazorApp/pull/3/conflict?name=BlazorApp%252Fwwwroot%252Fapi%252Fseeds%252Fseed.sql&base_oid=537402e333f8fd0923d763d1e038d4f2c2b4b546&head_oid=b3dd561faf93e60a93c725aa811f8f62923defea
     public class Program
     {
         public static async Task Main(string[] args)
@@ -17,7 +17,7 @@ namespace BlazorApp
             builder.Services.AddScoped<NavigationService>();
 
             builder.Services.AddSqliteWasmDbContextFactory<ThingContext>(
-                opts => opts.UseSqlite("Data Source=things.sqlite3"));
+                opts => opts.UseSqlite("Data Source=Things.sqlite3"));
 
             builder.Services.AddScoped(sp => new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
